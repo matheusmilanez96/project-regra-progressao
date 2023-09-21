a32f29b95c1e86bd465416ccacbefccd10398494
Matheus Milanez <mattmila96@gmail.com>
2023-09-21 20:53:08 -0300

    requisito 3

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,6 +1,8 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 /**
  * App.
@@ -15,6 +17,24 @@ public class App {
   }
 
   /**
+   * Método calcularNota.
+   */
+  public static float calcularNota(int[] notas, int[] pesos, int qtd) {
+
+    int[] multArr = { };
+    for (int n = 0; n < qtd; n += 1) {
+      int mult = notas[n] * pesos[n];
+      multArr = Arrays.copyOf(multArr, multArr.length + 1);
+      multArr[multArr.length - 1] = mult;
+    }
+
+    float somaMult = IntStream.of(multArr).sum();
+    float somaPesos = IntStream.of(pesos).sum();
+
+    return somaMult / somaPesos;
+  }
+
+  /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
@@ -23,17 +43,34 @@ public class App {
     System.out.println("Digite a quantidade de atividades para cadastrar: ");
     String qtdEntrada = scanner.nextLine();
     int qtd = Integer.parseInt(qtdEntrada);
+    int[] pesoArr = { };
+    int[] notaArr = { };
     for (int n = 1; n <= qtd; n += 1) {
       System.out.println("Digite o nome da atividade " + n + ": ");
       String nome = scanner.nextLine();
       System.out.println("Digite o peso da atividade " + n + ": ");
       String pesoEntrada = scanner.nextLine();
-      int peso = Integer.parseInt(pesoEntrada);
       System.out.println("Digite a nota obtida para " + nome + ": ");
+      int peso = Integer.parseInt(pesoEntrada);
+      pesoArr = Arrays.copyOf(pesoArr, pesoArr.length + 1);
+      pesoArr[pesoArr.length - 1] = peso;
       String notaEntrada = scanner.nextLine();
       int nota = Integer.parseInt(notaEntrada);
+      notaArr = Arrays.copyOf(notaArr, notaArr.length + 1);
+      notaArr[notaArr.length - 1] = nota;
     }
 
+    float notaFinal = calcularNota(notaArr, pesoArr, qtd);
+
+    if (notaFinal >= 85) {
+      System.out.println("Parabéns! Você alcançou " + notaFinal
+          + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+              + notaFinal
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+    }
     scanner.close();
   }
 }
