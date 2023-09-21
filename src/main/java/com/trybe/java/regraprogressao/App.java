6cb8fe598862197ace30283af8accfe7b8ee07d0
Matheus Milanez <mattmila96@gmail.com>
2023-09-21 18:05:59 -0300

    requisito 1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,27 @@ public class App {
    * Método main.
    */
   public static void main(String[] args) {
+    coletarInformacoes();
+  }
+
+  /**
+   * Método coletarInformacoes.
+   */
+  public static void coletarInformacoes() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    String qtdEntrada = scanner.nextLine();
+    int qtd = Integer.parseInt(qtdEntrada);
+    for (int n = 1; n <= qtd; n += 1) {
+      System.out.println("Digite o nome da atividade " + n + ": ");
+      String nome = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + n + ": ");
+      String pesoEntrada = scanner.nextLine();
+      int peso = Integer.parseInt(pesoEntrada);
+      System.out.println("O nome é " + nome + " e o peso é " + peso + ".");
+    }
+
+    scanner.close();
   }
 }
